dc14ff4ddc593e3330f3e2d72d79f1aa9a80d3ae
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-10-21 14:36:16 +0200

    Begin writing audio device locker

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { SyntheticEvent, useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [audioDevices, setAudioDevices] = useState<MediaDeviceInfo[]>([]);
+  const [selectedAudioOutputDevice, setSelectedAudioOutputDevice] =
+    useState<MediaDeviceInfo | null>();
+  const [selectedAudioInputDevice, setSelectedAudioInputDevice] =
+    useState<MediaDeviceInfo | null>();
+
+  useEffect(() => {
+    const getAudioDevices = async () => {
+      try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        setAudioDevices(devices);
+      } catch (error) {
+        console.error(`An error ocurred when fetching audio devices: ${error}`);
+      }
+    };
+
+    getAudioDevices();
+
+    const handleDeviceChange = () => {
+      console.log("Audio device changed.");
+      getAudioDevices();
+    };
+
+    navigator.mediaDevices.addEventListener("devicechange", handleDeviceChange);
+
+    return () => {
+      navigator.mediaDevices.removeEventListener(
+        "devicechange",
+        handleDeviceChange
+      );
+    };
+  }, []);
+
+  const lockAudioOutputDevice = (e: SyntheticEvent) => {
+    e.preventDefault();
+    console.log("Locking this one for output: ", selectedAudioOutputDevice);
+    console.log("Locking this one for input: ", selectedAudioInputDevice);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <form onSubmit={lockAudioOutputDevice}>
+      <label htmlFor="audio-devices">Audio Output devices</label>
+      <select
+        name="audio-devices"
+        id="audio-devices"
+        onChange={(e) =>
+          setSelectedAudioOutputDevice(
+            audioDevices.find((d) => d.label == e.target.value)
+          )
+        }
+      >
+        {audioDevices
+          .filter((device) => device.kind == "audiooutput")
+          .map((device) => (
+            <option key={device.deviceId}>{device.label}</option>
+          ))}
+      </select>
+      <br />
+      <label htmlFor="audio-devices">Audio Output devices</label>
+      <select
+        name="audio-devices"
+        id="audio-devices"
+        onChange={(e) =>
+          setSelectedAudioInputDevice(
+            audioDevices.find((d) => d.label == e.target.value)
+          )
+        }
+      >
+        {audioDevices
+          .filter((device) => device.kind == "audioinput")
+          .map((device) => (
+            <option key={device.deviceId}>{device.label}</option>
+          ))}
+      </select>
+      <br />
+      <button>Lock audio device(s)</button>
+    </form>
+  );
 }
 
-export default App
+export default App;
